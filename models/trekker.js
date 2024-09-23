0208b58dd11f01a892a6d0bacc3f26d75256ef12
Vilsium <vilsium28@gmail.com>
2024-09-23 23:03:40 +0530

    trekkerRoute.js updated for data sending

--- models/trekker.js
+++ models/trekker.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+const driverSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
   price: Number,
@@ -12,4 +12,4 @@ const productSchema = mongoose.Schema({
   maxCapacity: Number,
 });
 
-module.exports = mongoose.model("Driver_Data", productSchema);
+module.exports = mongoose.model("Driver_Data", driverSchema);
--- routes/trekkerRoutes.js
+++ routes/trekkerRoutes.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const Driver = require('../models/trekker');
 
 router.post("/driver_sign_up", async (req, res) => {
-  const {
-    driverName,
-    driverID,
-    driverEmail,
-    phoneNo,
-    password,
-    LicensePlate,
-    maxCapacity,
-  } = req.body;
+  const {driverName, driverID, driverEmail, phoneNo, password, LicensePlate, maxCapacity} = req.body;
 
   try {
     // Check if all required fields are provided (phoneNo is optional)
-    if (
-      driverName &&
-      driverID &&
-      driverEmail &&
-      password &&
-      LicensePlate &&
-      maxCapacity
-    ) {
+    if (!driverName || !driverID || !driverEmail || !phoneNo || !password || !LicensePlate || !maxCapacity) {
       res
-        .status(201)
-        .json({ msg: "The required data is available to the server" });
-    } else {
-      res.status(400).json({ msg: "The necessary data was not retrieved" });
+        .status(400)
+        .json({ msg: "Please fill all required fields" });
     }
+    const existingDriver = await Driver.findOne({driverEmail});
+    if(existingDriver) {
+      return res.status(400).json({msg:"Passenger with this email already exists"});
+    }
+    const newDriver = new Passenger({
+      _id: new mongoose.Types.ObjectId(),  // Manually assign an _id (optional)
+      drivername,
+      driverID,
+      driverEmail,
+      phoneNo,
+      password,
+      LicensePlate,
+      maxCapacity
+    })
+    await newDriver.save();
+    res.status(201).json({msg:"Passenger registered successfully"});
   } catch (error) {
     // Log the error and return server error response
     console.error(error);
