fde2d41f12c5afea1c741c7d5b2e8a5b7844722f
Vilsium <vilsium28@gmail.com>
2024-09-27 09:16:48 +0530

    pretty

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http')
-const app = require('./app')
+const http = require("http");
+const app = require("./app");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
 connectDB();
 
-const PORT = process.env.PORT || 3000
-const server = http.createServer(app)
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
 
-server.listen(PORT)
+server.listen(PORT);
