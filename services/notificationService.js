52f961407f99c09cf621e3fc47547163f9331026
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-10-07 22:15:33 +0530

    some logic related to websocket is applied but still not functioning as expected

---
+++ services/notificationService.js
@@ -0,0 +1,47 @@
+const WebSocket = require("ws");
+
+let clients = [];
+
+const initWebSocket = (server) => {
+  const wss = new WebSocket.Server({ server });
+
+  wss.on("connection", (ws) => {
+    console.log("New client connected");
+    clients.push(ws);
+    ws.send("Welcome new client");
+
+    ws.on("message", function incoming(message) {
+      try {
+        // Parse the incoming message to a JavaScript object (JSON format)
+        const parsedMessage = JSON.parse(message);
+        console.log("Received JSON:", parsedMessage);
+
+        const msg = parsedMessage.message; // "Hello"
+        const id = parsedMessage.id;
+
+        console.log(`Message: ${msg}, ID: ${id}`);
+
+        // Do something with the parsed JSON, like broadcasting it to other clients
+        broadcastMessage(msg, ws);
+      } catch (error) {
+        console.log("Error parsing message as JSON:", error);
+        ws.send(JSON.stringify({ error: "Invalid JSON format" })); // Send error message back if JSON is invalid
+      }
+    });
+    // Handle disconnection
+    ws.on("close", () => {
+      clients = clients.filter((client) => client !== ws);
+      console.log("Client disconnected");
+    });
+  });
+};
+
+const broadcastMessage = (message, ws) => {
+  clients.forEach((client) => {
+    if (client !== ws && client.readyState == WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+};
+
+module.exports = { initWebSocket, broadcastMessage };
