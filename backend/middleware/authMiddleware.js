742fe6745562f5ea6df2483ea0cfaf434c9c2788
Vilsium <vilsium28@gmail.com>
2024-10-13 12:06:34 +0530

    middleware added

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const authenticateToken = (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).send("Access Denied: No Token Provided");
+  }
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid Token");
+  }
+};
+
+module.exports = authenticateToken;
