817be2bdacb440c58850e3dd6b0f909ab2d70f48
Vilsium <vilsium28@gmail.com>
2024-10-13 14:23:38 +0530

    Login submit handled

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,49 +1,75 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-    const [show, setShow] = useState(false);
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const handleClick = () => setShow(!show);
+  const handleClick = () => setShow(!show);
 
-    const submitHandler = () => {};
+  const submitHandler = async () => {
+    try {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const { data } = await axios.post(
+            "http://localhost:5000/login",
+            { email, password },
+            config
+        );
+        console.log("Login Successful: ", data);
+        window.location.href = "/dashboard";
+    } catch(error) {
+        console.error("Login failed: ", error);
+    }
+  };
 
-    return (
-        <VStack spacing={'5px'} color={'black'}>
-            <FormControl id='email' isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input
-                    placeholder='Enter Your Email'
-                    onChange={(e)=>setEmail(e.target.value)}
-                />
-            </FormControl>
-            <FormControl id='password' isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                    <Input
-                        type={show ? 'text' : 'password'}
-                        placeholder='Enter Your Password'
-                        onChange={(e)=>setPassword(e.target.value)}
-                    />
-                    <InputRightElement>
-                        <Button h={'1.75rem'} size={'sm'} onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <Button
-                colorScheme='blue'
-                width={'100%'}
-                style={{marginTop: 15}}
-                onClick={submitHandler}
-            >
-                Log In
+  return (
+    <VStack spacing={"5px"} color={"black"}>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement>
+            <Button h={"1.75rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
             </Button>
-        </VStack>
-    )
-}
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width={"100%"}
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Log In
+      </Button>
+    </VStack>
+  );
+};
 
-export default Login
+export default Login;
