71f79ab36b1ffdb299265c9b970646ba52bdec98
Vilsium <vilsium28@gmail.com>
2024-10-13 12:23:40 +0530

    Signup form made according to database schema

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,81 +1,133 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Radio,
+  RadioGroup,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Signup = () => {
-    const [show, setShow] = useState(false);
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [sapid, setSapid] = useState();
-    const [password, setPassword] = useState();
-    const [confirmpassword, setConfirmpassword] = useState();
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [id, setId] = useState();
+  const [phone, setPhone] = useState();
+  const [role, setRole] = useState();
+  const [license, setLicense] = useState();
+  const [capacity, setCapacity] = useState();
+  const [password, setPassword] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
 
-    const handleClick = () => setShow(!show);
+  const handleClick = () => setShow(!show);
 
-    const submitHandler = () => {};
+  const submitHandler = () => {};
 
-    return (
-        <VStack spacing={'5px'} color={'black'}>
-            <FormControl id='first-name' isRequired>
-                <FormLabel>Name</FormLabel>
-                <Input
-                    placeholder='Enter Your Name'
-                    onChange={(e)=>setName(e.target.value)}
-                />
-            </FormControl>
-            <FormControl id='email' isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input
-                    placeholder='Enter Your Email'
-                    onChange={(e)=>setEmail(e.target.value)}
-                />
-            </FormControl>
-            <FormControl id='sapid' isRequired>
-                <FormLabel>SAP ID</FormLabel>
-                <Input
-                    placeholder='Enter SAP ID'
-                    onChange={(e)=>setSapid(e.target.value)}
-                />
-            </FormControl>
-            <FormControl id='password' isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                    <Input
-                        type={show ? 'text' : 'password'}
-                        placeholder='Enter Your Password'
-                        onChange={(e)=>setPassword(e.target.value)}
-                    />
-                    <InputRightElement>
-                        <Button h={'1.75rem'} size={'sm'} onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <FormControl id='confirmpassword' isRequired>
-                <FormLabel>Confirm Password</FormLabel>
-                <InputGroup>
-                    <Input
-                        type={show ? 'text' : 'password'}
-                        placeholder='Confirm Password'
-                        onChange={(e)=>setConfirmpassword(e.target.value)}
-                    />
-                    <InputRightElement>
-                        <Button h={'1.75rem'} size={'sm'} onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </FormControl>
-            <Button
-                colorScheme='blue'
-                width={'100%'}
-                style={{marginTop: 15}}
-                onClick={submitHandler}
-            >
-                Sign Up
+  return (
+    <VStack spacing={"5px"} color={"black"}>
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="id" isRequired>
+        <FormLabel>ID</FormLabel>
+        <Input
+          placeholder="Enter SAP ID / DL ID"
+          onChange={(e) => setId(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="phone" isRequired>
+        <FormLabel>Phone No</FormLabel>
+        <Input
+          placeholder="Enter Phone No"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </FormControl>
+      {/* <FormControl id="role" isRequired>
+        <FormLabel>Role (Driver or Passenger)</FormLabel>
+        <Input
+          placeholder="Enter Role"
+          onChange={(e) => setRole(e.target.value)}
+        />
+      </FormControl> */}
+      <FormControl as={"fieldset"}>
+        <FormLabel as="legend">Select Your Role</FormLabel>
+        <RadioGroup defaultValue="Passenger">
+          <HStack spacing="24px">
+            <Radio value="Driver">Driver</Radio>
+            <Radio value="Passenger">Passenger</Radio>
+          </HStack>
+        </RadioGroup>
+      </FormControl>
+      <FormControl id="license">
+        <FormLabel>License Plate No (for Driver Role)</FormLabel>
+        <Input
+          placeholder="Enter License Plate No"
+          onChange={(e) => setLicense(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="capacity">
+        <FormLabel>Enter Maximum Capacity (for Driver Role)</FormLabel>
+        <Input
+          placeholder="Enter Maximum Capacity"
+          onChange={(e) => setCapacity(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement>
+            <Button h={"1.75rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
             </Button>
-        </VStack>
-    )
-}
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="confirmpassword" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm Password"
+            onChange={(e) => setConfirmpassword(e.target.value)}
+          />
+          <InputRightElement>
+            <Button h={"1.75rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width={"100%"}
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
+};
 
-export default Signup
+export default Signup;
