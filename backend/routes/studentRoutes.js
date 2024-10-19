8b86f4fc21144faa25f67253348540088a9b8727
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-10-19 19:45:08 +0530

    removed login

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -4,26 +4,4 @@ const Passenger = require("../models/student");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await Passenger.findOne({ email });
-    if (!user) {
-      return res.status(400).send("User does not exist.");
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).send("Invalid password");
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.json({ token });
-  } catch (err) {
-    next(err);
-  }
-});
-
 module.exports = router;
