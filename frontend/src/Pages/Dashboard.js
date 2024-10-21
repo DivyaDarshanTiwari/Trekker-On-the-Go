73a22f3c3d18a9b3ffd8d018ccb15f431e3e8d48
Vilsium <vilsium28@gmail.com>
2024-10-21 10:59:50 +0530

    Redirecting to profile section when logging in

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -6,6 +6,7 @@ import {
   HStack,
   Button,
   Link,
+  Flex,
 } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import {
@@ -13,11 +14,13 @@ import {
   Link as RouterLink,
   Route,
   Routes,
+  useLocation,
 } from "react-router-dom";
 
 const Dashboard = () => {
   const [user, setUser] = useState({});
   const navigate = useNavigate(); // Use for redirecting if needed
+  const location = useLocation();
 
   useEffect(() => {
     // Get the logged-in user details from localStorage or redirect to login if no data
@@ -29,213 +32,220 @@ const Dashboard = () => {
       // If no user data found, redirect to login page
       navigate("/");
     }
-  }, [navigate]);
+
+    //Redirecting to profile when landing on Dashbaord
+    if (location.pathname === "/dashboard") {
+      navigate("/dashboard/profile");
+    }
+  }, [navigate, location]);
 
   return (
-    <VStack
-      spacing={6}
-      align="start"
-      p={8}
-      bg="gray.50"
-      w="full"
-      maxW="600px"
-      borderRadius="lg"
-      boxShadow="lg"
-    >
-      <Text fontSize="3xl" fontWeight="bold" color="teal.600" align="center">
-        User Dashboard
-      </Text>
-
-      {/* Navigation bar */}
-      <HStack
-        spacing={8}
+    <Flex justify={"center"} align={"center"} minH={"100vh"} bg={"gray.50"}>
+      <VStack
+        spacing={6}
+        align="start"
+        p={8}
+        bg="gray.50"
         w="full"
-        justifyContent="center"
-        p={4}
-        bg="teal.600"
-        color="white"
-        borderRadius="md"
+        maxW="600px"
+        borderRadius="lg"
+        boxShadow="lg"
       >
-        <Link
-          as={RouterLink}
-          to="/dashboard/profile"
-          fontWeight="bold"
-          _hover={{ textDecoration: "underline" }}
+        <Text fontSize="3xl" fontWeight="bold" color="teal.600" align="center">
+          User Dashboard
+        </Text>
+
+        {/* Navigation bar */}
+        <HStack
+          spacing={8}
+          w="full"
+          justifyContent="center"
+          p={4}
+          bg="teal.600"
+          color="white"
+          borderRadius="md"
         >
-          Profile
-        </Link>
-        <Link
-          as={RouterLink}
-          to="/dashboard/functionality"
-          fontWeight="bold"
-          _hover={{ textDecoration: "underline" }}
-        >
-          Functionality
-        </Link>
-      </HStack>
+          <Link
+            as={RouterLink}
+            to="/dashboard/profile"
+            fontWeight="bold"
+            _hover={{ textDecoration: "underline" }}
+          >
+            Profile
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/dashboard/functionality"
+            fontWeight="bold"
+            _hover={{ textDecoration: "underline" }}
+          >
+            Functionality
+          </Link>
+        </HStack>
 
-      {/* Divider for better visual separation */}
-      <Divider borderColor="gray.300" />
+        {/* Divider for better visual separation */}
+        <Divider borderColor="gray.300" />
 
-      <Routes>
-        <Route
-          path="/profile"
-          element={
-            <>
-              {/* Profile section showing user details */}
-              {user.name && (
-                <Box
-                  w="full"
-                  p={4}
-                  bg="white"
-                  borderRadius="md"
-                  boxShadow="md"
-                  borderColor="gray.200"
-                  borderWidth="1px"
-                >
-                  <Text fontSize="lg" fontWeight="bold">
-                    Name:
-                  </Text>
-                  <Text>{user.name}</Text>
-                </Box>
-              )}
-              {user.SAP_DL_ID && (
-                <Box
-                  w="full"
-                  p={4}
-                  bg="white"
-                  borderRadius="md"
-                  boxShadow="md"
-                  borderColor="gray.200"
-                  borderWidth="1px"
-                >
-                  <Text fontSize="lg" fontWeight="bold">
-                    ID:
-                  </Text>
-                  <Text>{user.SAP_DL_ID}</Text>
-                </Box>
-              )}
-              {user.Email && (
-                <Box
-                  w="full"
-                  p={4}
-                  bg="white"
-                  borderRadius="md"
-                  boxShadow="md"
-                  borderColor="gray.200"
-                  borderWidth="1px"
-                >
-                  <Text fontSize="lg" fontWeight="bold">
-                    Email:
-                  </Text>
-                  <Text>{user.Email}</Text>
-                </Box>
-              )}
-              {user.phoneNo && (
-                <Box
-                  w="full"
-                  p={4}
-                  bg="white"
-                  borderRadius="md"
-                  boxShadow="md"
-                  borderColor="gray.200"
-                  borderWidth="1px"
-                >
-                  <Text fontSize="lg" fontWeight="bold">
-                    Phone:
-                  </Text>
-                  <Text>{user.phoneNo}</Text>
-                </Box>
-              )}
-              {user.role && (
-                <Box
-                  w="full"
-                  p={4}
-                  bg="white"
-                  borderRadius="md"
-                  boxShadow="md"
-                  borderColor="gray.200"
-                  borderWidth="1px"
-                >
-                  <Text fontSize="lg" fontWeight="bold">
-                    Role:
-                  </Text>
-                  <Text>{user.role}</Text>
-                </Box>
-              )}
+        <Routes>
+          <Route
+            path="/profile"
+            element={
+              <>
+                {/* Profile section showing user details */}
+                {user.name && (
+                  <Box
+                    w="full"
+                    p={4}
+                    bg="white"
+                    borderRadius="md"
+                    boxShadow="md"
+                    borderColor="gray.200"
+                    borderWidth="1px"
+                  >
+                    <Text fontSize="lg" fontWeight="bold">
+                      Name:
+                    </Text>
+                    <Text>{user.name}</Text>
+                  </Box>
+                )}
+                {user.SAP_DL_ID && (
+                  <Box
+                    w="full"
+                    p={4}
+                    bg="white"
+                    borderRadius="md"
+                    boxShadow="md"
+                    borderColor="gray.200"
+                    borderWidth="1px"
+                  >
+                    <Text fontSize="lg" fontWeight="bold">
+                      ID:
+                    </Text>
+                    <Text>{user.SAP_DL_ID}</Text>
+                  </Box>
+                )}
+                {user.Email && (
+                  <Box
+                    w="full"
+                    p={4}
+                    bg="white"
+                    borderRadius="md"
+                    boxShadow="md"
+                    borderColor="gray.200"
+                    borderWidth="1px"
+                  >
+                    <Text fontSize="lg" fontWeight="bold">
+                      Email:
+                    </Text>
+                    <Text>{user.Email}</Text>
+                  </Box>
+                )}
+                {user.phoneNo && (
+                  <Box
+                    w="full"
+                    p={4}
+                    bg="white"
+                    borderRadius="md"
+                    boxShadow="md"
+                    borderColor="gray.200"
+                    borderWidth="1px"
+                  >
+                    <Text fontSize="lg" fontWeight="bold">
+                      Phone:
+                    </Text>
+                    <Text>{user.phoneNo}</Text>
+                  </Box>
+                )}
+                {user.role && (
+                  <Box
+                    w="full"
+                    p={4}
+                    bg="white"
+                    borderRadius="md"
+                    boxShadow="md"
+                    borderColor="gray.200"
+                    borderWidth="1px"
+                  >
+                    <Text fontSize="lg" fontWeight="bold">
+                      Role:
+                    </Text>
+                    <Text>{user.role}</Text>
+                  </Box>
+                )}
 
-              {/* Conditionally display driver details */}
-              {user.role === "Driver" && (
-                <>
-                  {user.LicensePlate && (
-                    <Box
-                      w="full"
-                      p={4}
-                      bg="white"
-                      borderRadius="md"
-                      boxShadow="md"
-                      borderColor="gray.200"
-                      borderWidth="1px"
-                    >
-                      <Text fontSize="lg" fontWeight="bold">
-                        License Plate:
-                      </Text>
-                      <Text>{user.LicensePlate}</Text>
-                    </Box>
-                  )}
-                  {user.maxCapacity && (
-                    <Box
-                      w="full"
-                      p={4}
-                      bg="white"
-                      borderRadius="md"
-                      boxShadow="md"
-                      borderColor="gray.200"
-                      borderWidth="1px"
-                    >
-                      <Text fontSize="lg" fontWeight="bold">
-                        Capacity:
-                      </Text>
-                      <Text>{user.maxCapacity}</Text>
-                    </Box>
-                  )}
-                </>
-              )}
-            </>
-          }
-        />
-        <Route
-          path="/functionality"
-          element={
-            <>
-              {/* Functionality section with buttons */}
-              <Box
-                w="full"
-                p={4}
-                bg="white"
-                borderRadius="md"
-                boxShadow="md"
-                borderColor="gray.200"
-                borderWidth="1px"
-              >
-                <Text fontSize="lg" fontWeight="bold" mb={4}>
-                  Functionality
-                </Text>
-                <Button colorScheme="teal" variant="solid" mb={2} w="full">
-                  Button 1
-                </Button>
-                <Button colorScheme="teal" variant="solid" mb={2} w="full">
-                  Button 2
-                </Button>
-                <Button colorScheme="teal" variant="solid" w="full">
-                  Button 3
-                </Button>
-              </Box>
-            </>
-          }
-        />
-      </Routes>
-    </VStack>
+                {/* Conditionally display driver details */}
+                {user.role === "Driver" && (
+                  <>
+                    {user.LicensePlate && (
+                      <Box
+                        w="full"
+                        p={4}
+                        bg="white"
+                        borderRadius="md"
+                        boxShadow="md"
+                        borderColor="gray.200"
+                        borderWidth="1px"
+                      >
+                        <Text fontSize="lg" fontWeight="bold">
+                          License Plate:
+                        </Text>
+                        <Text>{user.LicensePlate}</Text>
+                      </Box>
+                    )}
+                    {user.maxCapacity && (
+                      <Box
+                        w="full"
+                        p={4}
+                        bg="white"
+                        borderRadius="md"
+                        boxShadow="md"
+                        borderColor="gray.200"
+                        borderWidth="1px"
+                      >
+                        <Text fontSize="lg" fontWeight="bold">
+                          Capacity:
+                        </Text>
+                        <Text>{user.maxCapacity}</Text>
+                      </Box>
+                    )}
+                  </>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/functionality"
+            element={
+              <>
+                {/* Functionality section with buttons */}
+                <Box
+                  w="full"
+                  p={4}
+                  bg="white"
+                  borderRadius="md"
+                  boxShadow="md"
+                  borderColor="gray.200"
+                  borderWidth="1px"
+                >
+                  <Text fontSize="lg" fontWeight="bold" mb={4}>
+                    Functionality
+                  </Text>
+                  <Button colorScheme="teal" variant="solid" mb={2} w="full">
+                    Button 1
+                  </Button>
+                  <Button colorScheme="teal" variant="solid" mb={2} w="full">
+                    Button 2
+                  </Button>
+                  <Button colorScheme="teal" variant="solid" w="full">
+                    Button 3
+                  </Button>
+                </Box>
+              </>
+            }
+          />
+        </Routes>
+      </VStack>
+    </Flex>
   );
 };
 
