d3178da9059549e32e563634fe25203260d2111a
Vilsium <vilsium28@gmail.com>
2024-10-28 15:21:49 +0530

    prettier applied

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -30,17 +30,18 @@ const Signup = ({ setTabIndex }) => {
   const handleClick = () => setShow(!show);
 
   const submitHandler = async () => {
-
     //validating phone no
     const phoneNoRegEx = /^[0-9][10]$/;
-    if(!phoneNoRegEx.test(phoneNo)) {
-      alert("Phone number must be of 10 digits and should contain only numbers.")
+    if (!phoneNoRegEx.test(phoneNo)) {
+      alert(
+        "Phone number must be of 10 digits and should contain only numbers."
+      );
       return;
     }
 
     //validating license plate for driver
     const licensePlateRegEx = /^[A-Za-z]{2}[0-9]{2}[A-Za-z]{1,2}[0-9]{4}$/i;
-    if(role === "Driver" && !licensePlateRegEx.test(LicensePlate)) {
+    if (role === "Driver" && !licensePlateRegEx.test(LicensePlate)) {
       alert("License plate is not in proper format.");
       return;
     }
