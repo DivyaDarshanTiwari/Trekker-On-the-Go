84d80d7a016b0ea7b2257276509b6705f241be73
Vilsium <vilsium28@gmail.com>
2024-10-28 17:38:05 +0530

    List-Map structure ready

---
+++ backend/models/trekkerList.js
@@ -0,0 +1,73 @@
+//node for the doubly linked list, containing status and id
+class TrekkerNode {
+    constructor(trekkerId, status = 'available') {
+        trekkerId = this.trekkerId;
+        status = this.status;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+//list and map structure for operations
+class TrekkerList {
+    //initialising the list
+    constructor() {
+        this.trekkerMap = {}; //Hash-Map for fast lookups
+        this.head = null;
+        this.tail = null;
+    }
+
+    //adding trekker while logging in
+    addTrekker(trekkerId) {
+        const newTrekker = new TrekkerNode(trekkerId);
+        this.trekkerMap[trekkerId] = newTrekker;
+
+        //if it is an empty list
+        if(!this.head) {
+            this.head = this.tail = newTrekker;
+        } else {
+            newTrekker.next = this.head;
+            this.head.prev = newTrekker;
+            this.head = newTrekker;
+        }
+    }
+
+    //updating trekker status to `avaiable` or `On the Go`
+    updateTrekkerStatus(trekkerId, status) {
+        const trekkerNode = this.trekkerMap[trekkerId];
+        if(trekkerNode) trekkerNode.status = status;
+    }
+
+    //removing trekker from list on logging out
+    removeTrekker(trekkerId) {
+        const trekkerNode = this.trekkerMap[trekkerId];
+        if(!trekkerNode) return false;
+
+        //if head is to be deleted
+        if(trekkerNode === this.head) {
+            this.head = trekkerNode.next;
+            if(this.head) this.head.prev = null;
+        } else if(trekkerNode === this.tail) {
+            this.tail = trekkerNode.prev;
+            if(this.tail) this.tail.next = null;
+        } else {
+            trekkerNode.next.prev = trekkerNode.prev;
+            trekkerNode.prev.next = trekkerNode.next;
+        }
+        
+        //deleting from the hash-map
+        delete this.trekkerMap[trekkerId];
+        return true;
+    }
+
+    //printing all avaiable trekkers
+    getAvailableTrekkers() {
+        let current = this.head;
+        const availableTrekkers = [];
+        while(current) {
+            if(current.status === 'available') availableTrekkers.push(current.trekkerId);
+            current = current.next;
+        }
+        return availableTrekkers;
+    }
+}
